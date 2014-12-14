9e8667c1041d195d5945b6a629fecf94bcaa5d41
Claudio Cicali <claudio.cicali@gmail.com>
2014-12-14 19:42:38 +0100

    Includes the results in the module

--- index.js
+++ index.js
@@ -6,28 +6,53 @@ var Promise = require('bluebird'),
 var Response = function (html) {
 
     this.$ = cheerio.load(html);
+}
 
+var Bot = function () {
+
+    this.results = {
+        currentVersion: '',
+        releaseNotesUrl: '',
+        releaseDate: '',
+        downloadUrl: '',
+        downloadPage: ''
+    };
+}
+
+Bot.prototype.set = function (key, value) {
+    if (!this.results.hasOwnProperty(key)) {
+        throw new Error('Appdate bot: unknown result ' + key);
+    }
+    this.results[key] = value;
+}
+
+Bot.prototype.get = function (key) {
+    if (!this.results.hasOwnProperty(key)) {
+        throw new Error('Appdate bot: unknown result ' + key);
+    }
+    return this.results[key];
+}
+
+Bot.prototype.open = function (url) {
+
+    return new Promise(function (resolve, reject) {
+
+        request(url, function (error, response, body) {
+
+            if (error) {
+                return reject(error);
+            }
+
+            if (!(/^2/.test('' + response.statusCode))) {
+                return reject(response.statusCode);
+            }
+
+            resolve(new Response(body));
+        });
+    });
 }
 
 module.exports = {
-
-    open: function (url) {
-
-        return new Promise(function (resolve, reject) {
-
-            request(url, function (error, response, body) {
-
-                if (error) {
-                    return reject(error);
-                }
-
-                if (!(/^2/.test('' + response.statusCode))) {
-                    return reject(response.statusCode);
-                }
-
-                resolve(new Response(body));
-            });
-        });
-    }
+    Bot: Bot
 };
 
